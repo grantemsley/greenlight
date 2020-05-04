31fa6d076cc43a8ab1c45eb6df9c3cae803aedbc
David Pesce <davidpesce@gmail.com>
2020-05-04 11:11:19 -0400

    resolved remaining conflict

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -205,20 +205,14 @@ function showDeleteRoom(target) {
 function updateCurrentSettings(settings_path){
   // Get current room settings and set checkbox
   $.get(settings_path, function(room_settings) {
-    var settings = JSON.parse(room_settings) 
-<<<<<<< HEAD
-    $("#room_mute_on_join").prop("checked", settings.muteOnStart)
-    $("#room_require_moderator_approval").prop("checked", settings.requireModeratorApproval)
-    $("#room_anyone_can_start").prop("checked", settings.anyoneCanStart)
-    $("#room_all_join_moderator").prop("checked", settings.joinModerator)
-    $("#room_dial_number").val(settings.dialNumber)
-    $("#room_voice_bridge").val(settings.voiceBridge)
-=======
+    var settings = JSON.parse(room_settings)
+
     $("#room_mute_on_join").prop("checked", $("#room_mute_on_join").data("default") || settings.muteOnStart)
     $("#room_require_moderator_approval").prop("checked", $("#room_require_moderator_approval").data("default") || settings.requireModeratorApproval)
     $("#room_anyone_can_start").prop("checked", $("#room_anyone_can_start").data("default") || settings.anyoneCanStart)
     $("#room_all_join_moderator").prop("checked", $("#room_all_join_moderator").data("default") || settings.joinModerator)
->>>>>>> master
+    $("#room_dial_number").val(settings.dialNumber)
+    $("#room_voice_bridge").val(settings.voiceBridge)
   })
 }
 
